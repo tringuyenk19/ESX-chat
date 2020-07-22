f43814d91975a1d815e40877d34325a5c2272b16
tringuyenk19 <50008777+tringuyenk19@users.noreply.github.com>
2020-07-22 21:05:44 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ description 'chat management stuff'
 
 ui_page 'html/index.html'
 
-client_script "@lorraxs_AC/client/antiInject.lua"; client_script 'cl_chat.lua'
+client_script 'cl_chat.lua'
 server_script 'sv_chat.lua'
 
 files {
